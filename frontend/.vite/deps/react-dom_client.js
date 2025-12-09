fa6be72dd76d451b8c8c493bf3b14a2dba9c73d3
Martinhdeez <m.hernandezg@udc.es>
2025-12-09 20:46:23 +0100

    added .vite

---
+++ frontend/.vite/deps/react-dom_client.js
@@ -0,0 +1,7 @@
+import {
+  require_client
+} from "./chunk-6RGYNHIJ.js";
+import "./chunk-3HME3SJ2.js";
+import "./chunk-GH6UE3LJ.js";
+import "./chunk-G3PMV62Z.js";
+export default require_client();
