2a446b9ac1c054a86394e13488f2b57f6be30dac
samu-hr <samuel.hermida@udc.es>
2025-12-19 15:18:42 +0100

    polished dependencies and db migrations

--- docker/fastapi/entrypoint.sh
+++ docker/fastapi/entrypoint.sh
@@ -4,4 +4,10 @@ if [ ! -f "/config/.env" ]; then
     cp /build/.env_example /config/.env
 fi
 
+alembic upgrade head
+if [ $? -ne 0 ]; then
+    echo "Failed to run database migrations!"
+    exit 1
+fi
+
 exec "$@"
